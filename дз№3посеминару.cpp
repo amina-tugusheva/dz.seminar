1a83fca77b8aa68027620a3297c63a93eaaac0c0
amina-tugusheva <115834261+amina-tugusheva@users.noreply.github.com>
2022-10-20 23:23:41 +0300

    дз с 07.10.22 №3

---
+++ дз№3посеминару.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	float x, y;
+	cin >> x >> y;
+	if (((x * x) + (y * y) > 4) && (x > -1) && (x < 1) && (y > -3) && (y < 3))
+	{
+		cout << "попадает";
+	}
+	else if (((x * x) + (y * y) < 4) || (x < -1) || (x > 1) || (y < -3) || (y > 3))
+	{
+		cout << "не попадает";
+	}
+	else
+	{
+		cout << "граница";
+	}
+}
