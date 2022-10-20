9281d4059476dc98a120d8b020772130b5ec5775
amina-tugusheva <115834261+amina-tugusheva@users.noreply.github.com>
2022-10-20 23:21:58 +0300

    дз с 07.10.22 №1

---
+++ дз№1посеминару.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int d;
+	cin >> d;
+	switch (d)
+	{
+	case 1:
+		cout << "влево";
+		break;
+	case 2:
+		cout << "вверх";
+		break;
+	case 3:
+		cout << "вправо";
+		break;
+	case 4:
+		cout << "вниз";
+		break;
+	default:
+		cout << "no";
+
+
+	}
+}
