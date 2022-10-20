b8fd7ab57ca0b532090a85f8ac148c57fd1e74d1
amina-tugusheva <115834261+amina-tugusheva@users.noreply.github.com>
2022-10-20 23:22:50 +0300

    дз с 07.10.22 №1.б

---
+++ дз№1.б.посеминару.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int d;
+	cin >> d;
+	switch (d)
+	{
+	case 1:
+		cout << "горизонтальный";
+		break;
+	case 2:
+		cout << "вертикальный";
+		break;
+	case 3:
+		cout << "горизонтальный";
+		break;
+	case 4:
+		cout << "вертикальный";
+		break;
+	default:
+		cout << "no";
+
+
+	}
+}
