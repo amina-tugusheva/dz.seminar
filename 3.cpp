a90d09792a0e9bfe0c7af32973cafacd398bb33c
amina-tugusheva <115834261+amina-tugusheva@users.noreply.github.com>
2022-11-01 15:58:36 +0400

    дз с 21.10.22 (функции)

---
+++ 3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int f(int a)
+{
+	setlocale(LC_ALL, "ru");
+	if (a != 0)
+	{
+		a = -a;
+		return a;
+	}
+	else
+	{
+		return 0;
+	}
+}
+int main()
+{
+	int x;
+	cin >> x;
+	cout << f(x);
+}
