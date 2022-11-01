763edc4ada87129d69a1668e3ca04c74c3293f1f
amina-tugusheva <115834261+amina-tugusheva@users.noreply.github.com>
2022-11-01 15:00:27 +0400

    дз с 21.10.22(функции)

---
+++ 1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iomanip>
+#include <iostream>
+using namespace std;
+float f1(float s)
+{
+	float a;
+	a = sqrt(sqrt((16 * s * s) / 5.0));
+	return a;
+}
+float f2(float a)
+{
+	float b;
+	b = sqrt(a * a + a * a);
+	return b;
+}
+int main()
+{
+	float p, S, katet, gipotenuza;
+	cin >> S;
+	katet = f1(S);
+	gipotenuza = f2(katet);
+	cout << fixed << setprecision(2) << 2 * katet + gipotenuza;
+}
